986b8faf9778e28e79b36c16dac481d52e21a143
Siddharth Suresh <sid@Sureshs-MacBook-Air-2.local>
2021-05-12 12:17:33 +0530

    3 lives for Jumping Jack game

--- Jumping-Jack/app.js
+++ Jumping-Jack/app.js
@@ -9,7 +9,16 @@ var speedX = 0;
 var jackTheFrog = document.querySelector('.JackTheFrog');
 let startTimer;
 let distanceToMove;
+let livesRemaining = 3;
+var gameStatus = document.querySelector('.gameStatus')
+var life1 = document.querySelector('#life1')
+var life2 = document.querySelector('#life2')
+var life3 = document.querySelector('#life3')
+var deviceType = "keyboard"
 
+if ("ontouchstart" in document.documentElement) {
+  deviceType = "touch";
+}
 
 function moveFrog() {
 
@@ -36,16 +45,39 @@ function startJump(diff) {
 }
 
 function checkLanding(jackTop, jackBottom) {
-  jackTop = jackTop + 30;
-  jackBottom = jackBottom - 30;
+  jackTop = jackTop + 35;
+  jackBottom = jackBottom - 35;
   let lilypadrect = targetLilyPad.getBoundingClientRect();
   console.log('checkLanding', jackTop, jackBottom, lilypadrect.top, lilypadrect.bottom)
 
   if (jackTop > lilypadrect.bottom || (jackBottom < lilypadrect.top && jackTop < lilypadrect.top) ) {
     failParagraph.style.display = "block";
+    livesRemaining = livesRemaining - 1;
     resetJack();
   } else {
     successParagraph.style.display = "block";
+    nextLevel()
+  }
+  if (livesRemaining === 0) {
+    gameStatus.style.display = "block"
+    life1.style.display = "none"
+    life2.style.display = "none"
+    life3.style.display = "none"
+  }
+
+  if (livesRemaining === 1) {
+    life2.style.display = "none"
+    life3.style.display = "none"
+  }
+
+  if (livesRemaining === 2) {
+    life3.style.display = "none"
+  }
+
+  if (livesRemaining === 3) {
+    life1.style.display = "block"
+    life2.style.display = "block"
+    life3.style.display = "block"
   }
 }
 
@@ -54,30 +86,39 @@ function resetJack() {
   jackTheFrog.style.bottom = 20;
 }
 
+function nextLevel() {
+let lilypadrect = targetLilyPad.getBoundingClientRect();
+  if (successParagraph.style.display = "block") {
+    lilypadrect.bottom = 500;
+    lilypadrect.top = "";
+  }
+}
 
 
 //Event Listeners
+if (deviceType == "keyboard") {
+  jackTheFrog.addEventListener('mousedown', ()=> {
+    startTime = new Date()
 
-jackTheFrog.addEventListener('mousedown', ()=> {
-  startTime = new Date()
+  })
 
-})
+  jackTheFrog.addEventListener('mouseup', ()=> {
+    let diff = new Date() - startTime
+    distanceJumped.innerText = diff;
+    startJump(diff);
 
-jackTheFrog.addEventListener('mouseup', ()=> {
-  let diff = new Date() - startTime
-  distanceJumped.innerText = diff;
-  startJump(diff);
+  })
+} else {
+  jackTheFrog.addEventListener('ontouchstart', ()=> {
+    startTime = new Date()
 
-})
+  })
 
-jackTheFrog.addEventListener('ontouchstart', ()=> {
-  startTime = new Date()
+  jackTheFrog.addEventListener('ontouchend', ()=> {
+    let diff = new Date() - startTime
+    console.log('ontouchend', diff)
+    distanceJumped.innerText = diff;
+    startJump(diff);
 
-})
-
-jackTheFrog.addEventListener('ontouchend', ()=> {
-  let diff = new Date() - startTime
-  distanceJumped.innerText = diff;
-  startJump(diff);
-
-})
+  })
+}
