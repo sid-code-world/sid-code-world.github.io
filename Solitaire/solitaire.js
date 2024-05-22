64ed86026b4ce96d3ecdb8a1942bce95812750fb
sid-code-world <82712642+sid-code-world@users.noreply.github.com>
2024-05-22 22:30:54 +0900

    Add files via upload

--- Solitaire/solitaire.js
+++ Solitaire/solitaire.js
@@ -4,12 +4,10 @@ function Card(face, rank) {
     this.isFaceUp = false;
     this.pile = null;
 
- 
     this.color = function() {
         return (this.face === "Hearts" || this.face === "Diamonds") ? "red" : "black";
     };
 
-   
     this.isSameFace = function(otherCard) {
         return this.face === otherCard.face;
     };
@@ -22,19 +20,16 @@ function Card(face, rank) {
         return this.rank === otherCard.rank - 1;
     };
 
-   
     this.isNextInSequence = function(otherCard) {
         return this.rank === otherCard.rank + 1;
     };
 
-    
     this.display = function(container) {
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('card');
         if (this.isFaceUp) {
             cardDiv.classList.add('front');
-            cardDiv.setAttribute('data-face', this.face);
-            cardDiv.setAttribute('data-rank', this.rank);
+            cardDiv.style.backgroundPosition = `-${(this.rank - 1) * 72}px -${["Clubs", "Diamonds", "Hearts", "Spades"].indexOf(this.face) * 96}px`;
         } else {
             cardDiv.classList.add('back');
         }
@@ -42,33 +37,29 @@ function Card(face, rank) {
         cardDiv.addEventListener('dragstart', (e) => this.dragStart(e));
         cardDiv.addEventListener('dragend', (e) => this.dragEnd(e));
         container.appendChild(cardDiv);
-        this.element = cardDiv; 
+        this.element = cardDiv;
     };
 
-   
     this.dragStart = function(e) {
         e.dataTransfer.setData('text/plain', `${this.face},${this.rank}`);
         setTimeout(() => this.element.classList.add('invisible'), 0);
         this.pile.removeCard(this);
     };
 
-
     this.dragEnd = function(e) {
         this.element.classList.remove('invisible');
+        // Re-render the original pile to update its display
+        this.pile.render();
     };
 
-   
     this.setPile = function(pile) {
         this.pile = pile;
     };
 }
 
-
-
 function Deck() {
     this.cards = [];
 
-   
     this.initialize = function() {
         const faces = ["Hearts", "Diamonds", "Clubs", "Spades"];
         const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
@@ -80,7 +71,6 @@ function Deck() {
         }
     };
 
-   
     this.shuffle = function() {
         for (let i = this.cards.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -100,55 +90,48 @@ function Deck() {
     };
 }
 
-
 function Pile(container) {
     this.cards = [];
     this.container = container;
 
-   
     this.addCard = function(card) {
         this.cards.push(card);
         card.setPile(this);
         card.display(this.container);
     };
 
-    
     this.removeCard = function(card) {
         const index = this.cards.indexOf(card);
         if (index > -1) {
             this.cards.splice(index, 1);
+            this.container.removeChild(card.element);
         }
     };
 
-    
     this.canPlaceCard = function(card) {
         if (this.cards.length === 0) {
-            return true; 
+            return true;
         } else {
             const topCard = this.cards[this.cards.length - 1];
             return card.isOppositeColor(topCard) && card.isPreviousInSequence(topCard);
         }
     };
 
-    
     this.initialize = function(deck, numCards) {
         for (let i = 0; i < numCards; i++) {
             const card = deck.deal();
-            card.isFaceUp = (i === numCards - 1); 
+            card.isFaceUp = (i === numCards - 1);
             this.addCard(card);
         }
     };
 
-    
     this.container.addEventListener('dragover', (e) => this.dragOver(e));
     this.container.addEventListener('drop', (e) => this.drop(e));
 
-    
     this.dragOver = function(e) {
         e.preventDefault();
     };
 
-    
     this.drop = function(e) {
         const data = e.dataTransfer.getData('text/plain').split(',');
         const card = new Card(data[0], parseInt(data[1]));
@@ -156,14 +139,15 @@ function Pile(container) {
             card.isFaceUp = true;
             this.addCard(card);
         } else {
-          
             card.pile.addCard(card);
         }
     };
+
+    this.render = function() {
+        this.container.innerHTML = '';
+        this.cards.forEach(card => card.display(this.container));
+    };
 }
-
-
-
 
 function FoundationPile(container) {
     this.cards = [];
@@ -171,30 +155,28 @@ function FoundationPile(container) {
 
     this.canPlaceCard = function(card) {
         if (this.cards.length === 0) {
-            return card.rank === 1; 
+            return card.rank === 1;
         } else {
             const topCard = this.cards[this.cards.length - 1];
             return card.isSameFace(topCard) && card.isNextInSequence(topCard);
         }
     };
 
-  
     this.addCard = function(card) {
         this.cards.push(card);
         card.setPile(this);
-        card.isFaceUp = true; 
+        card.isFaceUp = true;
         card.display(this.container);
     };
 
-    
     this.removeCard = function(card) {
         const index = this.cards.indexOf(card);
         if (index > -1) {
             this.cards.splice(index, 1);
+            this.container.removeChild(card.element);
         }
     };
 
-  
     this.container.addEventListener('dragover', (e) => this.dragOver(e));
     this.container.addEventListener('drop', (e) => this.drop(e));
 
@@ -202,7 +184,6 @@ function FoundationPile(container) {
         e.preventDefault();
     };
 
-   
     this.drop = function(e) {
         const data = e.dataTransfer.getData('text/plain').split(',');
         const card = new Card(data[0], parseInt(data[1]));
@@ -210,26 +191,35 @@ function FoundationPile(container) {
             card.isFaceUp = true;
             this.addCard(card);
         } else {
-            
             card.pile.addCard(card);
         }
     };
-}
 
+    this.render = function() {
+        this.container.innerHTML = '';
+        this.cards.forEach(card => card.display(this.container));
+    };
+}
 
 function DiscardPile(container) {
     this.cards = [];
     this.container = container;
 
-   
     this.addCard = function(card) {
         this.cards.push(card);
         this.render();
     };
 
-    
+    this.removeCard = function(card) {
+        const index = this.cards.indexOf(card);
+        if (index > -1) {
+            this.cards.splice(index, 1);
+            this.container.removeChild(card.element);
+        }
+    };
+
     this.render = function() {
-        this.container.innerHTML = ''; 
+        this.container.innerHTML = '';
         if (this.cards.length > 0) {
             const topCard = this.cards[this.cards.length - 1];
             topCard.isFaceUp = true;
@@ -238,21 +228,16 @@ function DiscardPile(container) {
     };
 }
 
-
-
-
 function Game() {
     this.deck = new Deck();
     this.piles = [];
     this.foundationPiles = [];
     this.discardPile = null;
 
-
     this.initialize = function() {
         this.deck.initialize();
         this.deck.shuffle();
 
-    
         for (let i = 0; i < 7; i++) {
             const pileContainer = document.getElementById(`pile${i + 1}`);
             const pile = new Pile(pileContainer);
@@ -260,7 +245,6 @@ function Game() {
             this.piles.push(pile);
         }
 
-      
         for (let i = 0; i < 4; i++) {
             const foundationContainer = document.getElementById(`foundationPile${i + 1}`);
             this.foundationPiles.push(new FoundationPile(foundationContainer));
@@ -278,12 +262,10 @@ function Game() {
         });
     };
 
- 
     this.start = function() {
         this.initialize();
     };
 }
-
 
 const game = new Game();
 game.start();
